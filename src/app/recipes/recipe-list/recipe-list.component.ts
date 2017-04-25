c041617c64b29a541354167383e899915a568240
fdbiondi <fdbion@gmail.com>
2017-04-25 01:04:26 -0300

    Recipes component comunication

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -8,14 +8,19 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   recipes: Recipe[] = [
     new Recipe('A Test Recipe', 'This is a test recipe', 'http://www.foodtolove.com.au/assets/images/badge-collection.png'),
-    new Recipe('A Test Recipe', 'This is a test recipe', 'http://www.foodtolove.com.au/assets/images/badge-collection.png')
+    new Recipe('Another Test Recipe', 'This is a test recipe', 'http://www.foodtolove.com.au/assets/images/badge-collection.png')
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
